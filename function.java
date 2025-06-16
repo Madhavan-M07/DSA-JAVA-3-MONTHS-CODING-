b8d41e4c5b1d83cd5a0bc60c0b8ab115d2423199
Madhavan M <madhavan09072003@gmail.com>
2025-06-16 22:14:16 +0530

    Initial commit

---
+++ function.java
@@ -0,0 +1,13 @@
+public class function {
+    public static void main(String[] args){
+        System.out.println("Java Program Skelton");
+        greetUser("Madhavan");
+    }
+    
+    public static void greetUser(String s){
+
+        System.out.println("Hello, " + s + "!");
+        
+    }
+
+}
